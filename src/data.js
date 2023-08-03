b7dbdb6883ee9f2293520dddb3af09b132ed1d51
Luis Miguel Barthaburu <barthaburuluismiguel@outlook.com>
2023-08-03 19:56:11 -0300

    carga de proyectos y actualizacion de cv

--- src/data.js
+++ src/data.js
@@ -1,10 +1,30 @@
-import tNoa from '../src/Assets/Img/turismo-noa.png'
+// import tNoa from '../src/Assets/Img/turismo-noa.png'
 import toDo from '../src/Assets/Img/to-do-list.png'
 import cpd from '../src/Assets/Img/campoder.png'
-import migo from '../src/Assets/Img/migo.png'
+// import migo from '../src/Assets/Img/migo.png'
 import crypto from '../src/Assets/Img/crypto.png'
 import clinica from '../src/Assets/Img/clinica-rc.png'
+import toolman from '../src/Assets/Img/toolman.png'
+import taller from '../src/Assets/Img/miTaller.png'
+
+
 export const portfolio =[
+  {
+    id:8,
+    img:taller,
+    nombre: 'Mi Taller (W.I.P.)',
+    repositorio:'https://github.com/LMBarthaburu/taller',
+    link: 'https://joyful-genie-b9be85.netlify.app/',
+    tecnologias:'MERN '
+  },
+  {
+    id:7,
+    img:toolman,
+    nombre: 'Toolman (W.I.P.)',
+    repositorio:'https://github.com/LMBarthaburu/toolman',
+    link: 'https://harmonious-mochi-a38f1d.netlify.app/',
+    tecnologias:'REACT, Bootstrap'
+  },
   {
     id: 5,
     img:cpd,
@@ -22,20 +42,13 @@ export const portfolio =[
     tecnologias:'HTML, CSS, Bootstrap, Javascript, React',
   },
   {
-    id: 2,
-    img:migo,
-    nombre: 'Migo Community',
-    repositorio:'https://github.com/LMBarthaburu/red-social',
-    link:'https://migo-community.netlify.app/',
-    tecnologias:'HTML, CSS, Javascript',
-  },
-  {
-    id: 3,
-    nombre: 'Turismo NOA',
-    img:tNoa,
-    repositorio:'https://github.com/LMBarthaburu/turismo-noa',
-    link:'https://turismo-noa.netlify.app/',
-    tecnologias:'HTML, CSS, Bootstrap',
+    id: 6,
+    img:clinica,
+    nombre: 'Clinica RC',
+    repositorio:'https://github.com/marcosarias21/proyecto-clinica',
+    repositorioBackEnd: 'https://github.com/LMBarthaburu/proyecto-clinica-backend',
+    link:'https://clinica-rc-24i.netlify.app/',
+    tecnologias:'MERN',
   },
   {
     id: 4,
@@ -45,13 +58,21 @@ export const portfolio =[
     link:'https://todo-list-app-example.netlify.app/',
     tecnologias:'HTML, CSS, Javascript',
   },
-  {
-    id: 6,
-    img:clinica,
-    nombre: 'Clinica RC',
-    repositorio:'https://github.com/marcosarias21/proyecto-clinica',
-    repositorioBackEnd: 'https://github.com/LMBarthaburu/proyecto-clinica-backend',
-    link:'https://clinica-rc-24i.netlify.app/',
-    tecnologias:'MERN',
-  },
+  // {
+  //   id: 2,
+  //   img:migo,
+  //   nombre: 'Migo Community',
+  //   repositorio:'https://github.com/LMBarthaburu/red-social',
+  //   link:'https://migo-community.netlify.app/',
+  //   tecnologias:'HTML, CSS, Javascript',
+  // },
+  // {
+  //   id: 3,
+  //   nombre: 'Turismo NOA',
+  //   img:tNoa,
+  //   repositorio:'https://github.com/LMBarthaburu/turismo-noa',
+  //   link:'https://turismo-noa.netlify.app/',
+  //   tecnologias:'HTML, CSS, Bootstrap',
+  // },
+
 ]
