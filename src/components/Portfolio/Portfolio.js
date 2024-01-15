58daf9187d24f4e2425f89f7b8d2c3e10ad077a8
Luis Miguel Barthaburu <barthaburuluismiguel@outlook.com>
2024-01-15 22:03:48 +0100

    cambio en el estilo de las card del portfolio y agregue descripcion de los proyectos

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -8,7 +8,7 @@ function Portfolio() {
     <section className='portfolio' id='portfolio'>
       <h2 className='portfolio-title'>Portfolio</h2>
       <div className='portfolio-proyects'>
-        {portfolio.map(item=> <CardPortfolio key={item.id} titulo={item.nombre} link={item.link} repositorio={item.repositorio} img={item.img} tecnologias={item.tecnologias}/>)}
+        {portfolio.map(item=> <CardPortfolio key={item.id} titulo={item.nombre} link={item.link} repositorio={item.repositorio} img={item.img} tecnologias={item.tecnologias} descripcion={item.descripcion}/>)}
       </div>
     </section>
   )
